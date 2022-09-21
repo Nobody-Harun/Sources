bf7e984f00f711ac5bc4fa721a1b3817698d8746
ChromePro <77278342+harunLog@users.noreply.github.com>
2022-09-21 20:04:31 +0900

    Fix "Test"

--- 回避/scripts/disabler.js
+++ 回避/scripts/disabler.js
@@ -6,13 +6,14 @@ if (!(location.host == "chrome.google.com" && location.pathname.startsWith("/web
     switch (window.navigator.language) {
         case "ja":
             alert("https://chrome.google.com/webstorexへ移動し終わった後もう一度、このブックマークレートをクリックしてください。");
+			location.href = "https://chrome.google.com/webstorex";
             break;
 
         default:
             alert("IMPORTANT PLS READ: After clicking OK, if it takes you to a 404, you are good. Click the bookmarklet once more after doing that.");
+			location.href = "https://chrome.google.com/webstorex";
             break;
     };
-    location.href = "https://chrome.google.com/webstorex";
   }
 
   document.head.innerHTML = `
